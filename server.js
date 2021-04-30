c2b1d3f61f8e0c90b27e2ccf3c178f010bd09bc7
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-04-30 11:05:45 +0200

    added create account-function

--- server.js
+++ server.js
@@ -9,8 +9,35 @@ const app = express();
 app.use(express.json());
 
 function initiateDatabase() {
-  database.defaults({ menu: [] }).write();
+  database.defaults({ menu: [] }, { account: [] }).write();
 }
+
+// /api/account 	POST 	Skapar ett användarkonto
+app.post("/api/account", (request, response) => {
+  const account = request.body;
+  console.log("Konto att lägga till", account);
+
+  const userNameExists = database
+    .get("account")
+    .find({ username: account.username })
+    .value();
+
+  console.log("usernameExits:", userNameExists);
+
+  const result = {
+    success: false,
+    userNameExists: false,
+  };
+
+  if (userNameExists) {
+    result.userNameExists = true;
+  }
+  if (!result.userNameExists) {
+    database.get("account").push(account).write();
+    result.success = true;
+  }
+  response.json(result);
+});
 
 app.listen(8000, () => {
   console.log("server started");

8e779ee154d16e381ad43ab685d02428db365679
Shrujana Peesari <shrujana.peesari@iths.se>
2021-05-01 20:37:21 +0200

    added coffee menu

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const lowdb = require("lowdb");
 const express = require("express");
 const FileSync = require("lowdb/adapters/FileSync");
-
 const adapter = new FileSync("menu.json");
 const database = lowdb(adapter);
 
@@ -11,6 +10,12 @@ app.use(express.json());
 function initiateDatabase() {
   database.defaults({ menu: [] }).write();
 }
+
+ app.get('/api/coffee', (req, res) => {
+  const menu = require('./menu.json');
+  res.json(menu);
+});
+
 
 app.listen(8000, () => {
   console.log("server started");

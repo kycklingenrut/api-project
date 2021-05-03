ee5df821bc97d8001cb615319be9a6ca2e3a02c9
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-05-03 13:09:51 +0200

    fixes to order history and fiddle with orders

--- server.js
+++ server.js
@@ -13,6 +13,48 @@ function initiateDatabase() {
   database.defaults({ menu: [] }, { orders: [] }, { accounts: [] }).write();
 }
 
+// To add orders
+app.post("/api/order", (req, res) => {
+  const menuItem = req.body;
+  // menuItem.id = nanoid();
+  const coffeeItem = database
+    .get("menu")
+    .find({ title: menuItem.title })
+    .value();
+  console.log(coffeeItem);
+  const userStatus = database
+    .get("accounts")
+    .find({ userId: menuItem.id })
+    .value();
+
+  const result = {
+    success: false,
+    menuItem: false,
+    userStatus: false,
+  };
+  if (!result.coffeeItem || !result.userStatus) {
+    result.success = false;
+    result.message =
+      "The item doesn't exist in the menu or you got the wrong account";
+  }
+  if (coffeeItem && userStatus) {
+    result.success = true;
+    result.menuItem = true;
+    result.userStatus = true;
+    database
+      .get("orders")
+      .push({
+        id: coffeeItem.id,
+        title: coffeeItem.title,
+        price: coffeeItem.price,
+        userId: menuItem.id,
+        orderNumber: nanoid(3),
+      })
+      .write();
+  }
+  res.json(result);
+});
+
 // Creating a user-account
 app.post("/api/account", (req, res) => {
   const account = req.body;
@@ -40,7 +82,7 @@ app.post("/api/account", (req, res) => {
     database
       .get("accounts")
       .push({
-        userId: nanoid(10),
+        id: nanoid(10),
         username: account.username,
         password: account.password,
       })
@@ -55,17 +97,21 @@ app.get("/api/order/:id", (req, res) => {
   const orderDetails = req.params.id;
   console.log("Order details:", orderDetails);
 
-  const allOrders = database.get("order").value();
+  const allOrders = database
+    .get("orders")
+    .filter({ userId: orderDetails })
+    .value();
+  console.log(allOrders);
 
   let result = {};
   if (allOrders.length > 0) {
     result.success = true;
   } else {
     result.success = false;
-    result.message = "This order doesn't exist yet";
+    result.message = "You have not placed an order yet";
   }
 
-  res.json(result);
+  res.json(allOrders);
 });
 app.listen(8000, () => {
   console.log("server started");

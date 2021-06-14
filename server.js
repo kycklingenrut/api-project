e3840346b059ace395f2e43d53a359a78f2af0ce
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-06-14 12:34:03 +0200

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { initiateDatabase } = require("./handlers/db-handlers");
 const coffeeRoutes = require("./routes/routes");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", coffeeRoutes);
 
